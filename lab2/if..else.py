4ba04274a60ffb03d79c2252f79d54461c332869
Ismail <ismailapabek1@gmail.com>
2024-01-30 22:05:22 +0600

    ifs

---
+++ lab2/if..else.py
@@ -0,0 +1,53 @@
+#task1
+a = 50
+b = 10
+if a > b:
+    print("Hello World") #output:Hello World
+#task2
+a = 50
+b = 10
+if a != b:
+    print("Hello World") #output:Hello World
+#task3
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No") #output:No
+#task4
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2") #output:2
+else:
+    print("3")
+#task5
+a = 1
+b = 1
+c = 2
+d = 2
+if a == b and c == d:
+    print("Hello") #output:Hello
+#task6
+a = 11
+b = 11
+c = 44
+d = 43
+if a == b or c == d:
+    print("Hello") #output:Hello
+#task7
+if 5 > 2:
+    print("YES") #output:YES
+#task8
+a = 2
+b = 5
+print("YES") if a > b else print("NO") #output:NO
+#task8
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES") #output:YES
