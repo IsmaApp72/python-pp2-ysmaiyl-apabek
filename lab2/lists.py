ce657227b9c6dc1dce8ba92822b3b2284fe91613
Ismail <ismailapabek1@gmail.com>
2024-01-30 16:32:27 +0600

    lists

---
+++ lab2/lists.py
@@ -0,0 +1,30 @@
+#task1
+fruits = ["apple","banana","cherry"]
+print(fruits[1]) #output:banana
+#task2
+fruits = ["apple","banana","cherry"]
+fruits[0]="kiwi"
+print(fruits[0]) #output:kiwi
+#task3
+fruits = ["apple","banana","cherry"]
+fruits.append("orange")
+print(fruits) #output:['apple','banana','cherry','orange']
+#task4
+fruits = ["apple","banana","cherry"]
+fruits.insert(1,"lemon")
+print(fruits)  #output:['apple','lemon','banana','cherry']
+#task5
+fruits = ["apple","banana","cherry"]
+fruits.remove("banana")
+print(fruits) #output:['apple','cherry']
+#task6
+fruits = ["apple","banana","cherry"]
+print(fruits[-1]) #output:cherry
+#task7
+fruits = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(fruits[2:5]) #output:['cherry','orange,'kiwi']
+#task8
+fruits = ["apple","banana","cherry"]
+print(len(fruits)) #output:3
+
+
