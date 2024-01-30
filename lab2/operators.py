4d940f8aa54d32f2d52ffdc5733a3eab93fd3d4a
Ismail <ismailapabek1@gmail.com>
2024-01-30 16:12:24 +0600

    operator

---
+++ lab2/operators.py
@@ -0,0 +1,14 @@
+#task1
+print(10*5) #output:50
+#task2
+print(10/2) #output:5.0
+#task3
+fruits = ["apple","banana"]
+if "apple" in fruits:
+    print("Yes,apple is a fruit!") #output:Yes,apple is a fruit!
+#task4
+if 5!=10:
+    print("5 and 10 is not equal") #output:5 and 10 is not equal
+#task5
+if 5==10 or 4==4:
+    print("At Least one of the statements is true") #output:At Least one of the statements is true
